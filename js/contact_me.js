daf9ce4eec19c5ea22f53538c1033b51a4dc2e8a
Alex Munda <alex.munda3@gmail.com>
2016-04-15 06:51:24 -0500

    Calling API to send email

--- js/contact_me.js
+++ js/contact_me.js
@@ -13,7 +13,6 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
@@ -21,14 +20,15 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "https://treovwr40l.execute-api.us-west-2.amazonaws.com/prod/sendContactEmail",
                 type: "POST",
-                data: {
+                data: JSON.stringify({
                     name: name,
-                    phone: phone,
                     email: email,
                     message: message
-                },
+                }),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
                 cache: false,
                 success: function() {
                     // Enable button & show success message
